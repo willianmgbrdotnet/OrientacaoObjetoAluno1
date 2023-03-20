7bd4dbb005d20bce21cbe14ee200aa304d598c30
WillianMGBrdotnet <willianmgbr@hotmail.com>
2023-03-19 21:28:12 -0300

    objetos e Metodos implementados

--- Aluno.cs
+++ Aluno.cs
@@ -2,6 +2,13 @@ namespace OrientacaoObjetoAluno1
 {
     public class Aluno
     {
-        
+        public string? Nome;
+        public int Idade;
+        public int Serie;
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"O aluno {Nome} tem {Idade} anos e pertence ao {Serie} ano.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
-﻿class Program
+﻿using OrientacaoObjetoAluno1;
+
+class Program
 {
     static void Main(string[] args)
     {
-        
+        Aluno aluno1 = new Aluno();
+        aluno1.Nome = "John";
+        aluno1.Idade = 18;
+        aluno1.Serie = 8;
+        aluno1.Apresentar();
+
+        Aluno aluno2 = new Aluno();
+        aluno2.Nome = "Mary";
+        aluno2.Idade = 20;
+        aluno2.Serie = 9;
+        aluno2.Apresentar();
+
     }
 }
